92266b1acc114c3b6a63f31e240bbee2191760c2
abs960 <a.b.s960@gmail.com>
2017-05-04 21:15:06 +0300

    Added window initialization. Added Shader class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,102 @@
-#include <iostream>
+#define START_WIDTH 800
+#define START_HEIGHT 600
 
-int main() {
+#include <iostream>
+#include <cmath>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+
+// Function prototypes
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode);
+void MouseCallback(GLFWwindow *window, double xPos, double yPos);
+void ScrollCallback(GLFWwindow *window, double xOffset, double yOffset);
+
+int initWindow();
+
+const char* window_title = "Let there be Light!";
+
+GLint screen_width, screen_height;
+
+int main(int argc, const char * argv[]) {
+    if (EXIT_SUCCESS != initWindow()) {
+        return EXIT_FAILURE;
+    }
+
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    return EXIT_SUCCESS;
+}
+
+int initWindow() {
+    // Init GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(START_WIDTH, START_HEIGHT, window_title, nullptr, nullptr);
+
+    if (nullptr == window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwGetFramebufferSize(window, &screen_width, &screen_height);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback(window, MouseCallback);
+    glfwSetScrollCallback(window, ScrollCallback);
+
+    // GLFW Options
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (GLEW_OK != glewInit()) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+
+        return EXIT_FAILURE;
+    }
+
+    // Define the viewport dimensions
+    glViewport(0, 0, screen_width, screen_height);
+
+    // OpenGL options
+    glEnable(GL_DEPTH_TEST);
+
+    return EXIT_SUCCESS;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode) {
+
+}
+
+void MouseCallback(GLFWwindow *window, double xPos, double yPos) {
+
+}
+
+void ScrollCallback(GLFWwindow *window, double xOffset, double yOffset) {
+
 }
